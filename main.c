38223a79b71a1bcc1007f4c6a843d45dcfbb370b
Yulai202020 <ngmyulai@gmail.com>
2024-06-13 15:38:24 +0400

    first commit

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    char *message = "";
+
+    if (argc == 1) {
+        message = "y";
+    } else if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            char addiction;
+
+            if (i + 1 == argc) {
+                addiction = '\0';
+            } else {
+                addiction = ' ';
+            }
+
+            char *tmp = argv[i];
+
+            size_t source_len = strlen(tmp);
+            char *tmp2 = (char *)malloc(source_len+1);
+            strcpy(tmp2, tmp);
+
+            tmp2[source_len] = addiction;
+            tmp2[source_len+1] = '\0';
+
+            size_t tmp2_len = strlen(tmp2);
+            size_t message_len = strlen(message);
+            size_t total_len = tmp2_len + message_len + 1;
+
+            char *result = (char *)malloc(total_len*sizeof(char));
+
+            strcpy(result, message);
+            strcat(result, tmp2);
+
+            message = (char *)malloc(strlen(result));
+            strcpy(message, result);
+        }
+    }
+
+    for (;;) {
+        printf("%s\n", message);
+    }
+
+    return 0;
+}
